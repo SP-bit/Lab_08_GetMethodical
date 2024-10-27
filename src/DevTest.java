ee52e5c18a6fe4311af4f0e3c9327b409608dde3
Sathwik <ps.bits5@gmail.com>
2024-10-27 16:36:03 -0400

    1st method

--- src/DevTest.java
+++ src/DevTest.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
 
 public class DevTest {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+
+        String result = SafeInput.getNonZeroLenString(in, "Enter a non-empty string");
+        System.out.println("You entered: " + result);
+    }
 }
--- src/SafeInput.java
+++ src/SafeInput.java
@@ -1,4 +1,43 @@
-package PACKAGE_NAME;
+import java.util.Scanner;
+public class SafeInput
+{
+    /**
+     *
+     * @param pipe a Scanner opened to read from System.in
+     * @param prompt prompt for the user
+     * @return a String response that is not zero length
+     */
+    public static String getNonZeroLenString(Scanner pipe, String prompt)
+    {
+        String retString = ""; // Set this to zero length. Loop runs until it isn't
+        do
+        {
+            System.out.print("\n" +prompt + ": "); // show prompt add space
+            retString = pipe.nextLine();
+        }while(retString.length() == 0);
 
-public class SafeInput {
+        return retString;
+
+    }
+    public static int getInt(Scanner pipe, String prompt)
+    {
+        int userInput = 0;
+        boolean valid = false;
+        String trash = "";
+        while (!valid) {
+
+            System.out.print(prompt + ": ");
+            if (pipe.hasNextInt()) {
+                userInput = pipe.nextInt();
+                pipe.nextLine();
+                valid = true;
+            } else {
+                trash = pipe.nextLine();
+                System.out.println("\nYou entered: " + trash);
+                System.out.println("You have to enter a valid integer!");
+            }
+        }
+        return userInput;
+    }
+
 }
